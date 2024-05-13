8c5bb8c7f5b37062a1978efd7c3352ff9b98c513
TylerVermeulen <36793@ma-web.nl>
2024-05-13 12:53:14 +0200

    List opdrachten gedaan

---
+++ ConsoleMonGame/ConsoleMonGame/ConsoleMon.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleMonGame
+{
+    internal class ConsoleMon
+    {
+        internal string name;
+        internal int energy;
+        internal int health;
+
+        private List<Skill> skills = new List<Skill>();
+
+        internal void TakeDamage(int damage)
+        {
+            health = health - damage;
+        }
+        internal void DepleteEnergy(int energy)
+        {
+            this.energy = this.energy - energy;
+        }
+    }
+}
